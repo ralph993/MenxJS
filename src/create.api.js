fe7a2014cee31a58f12703b263112fb017eda0e1
ralph993 <77635442+ralph993@users.noreply.github.com>
2021-05-12 10:33:46 -0400

    last

--- src/create.api.js
+++ src/create.api.js
@@ -1,3 +1,40 @@
+import inquirer from "inquirer";
+import { mkdir, mkdirSync, appendFile } from "fs";
+import Listr from "listr";
+
+const cwd = process.cwd();
+
+const apiDir = `${cwd}/src/api`;
+
+function createController(apiName) {
+  const template = "module.exports = {};";
+
+  appendFile(
+    `${apiDir}/${apiName}/${apiName}.controller.js`,
+    template,
+    (err) => {
+      if (err) throw err;
+    }
+  );
+}
+
 export async function generateApiComponent() {
-  console.log("start generator");
+  const { apiName } = await inquirer.prompt({
+    type: "input",
+    message: "Component Name",
+    name: "apiName",
+  });
+
+  const task = new Listr([
+    {
+      title: "Create component folder",
+      task: () => mkdirSync(`${apiDir}/${apiName}`),
+    },
+    {
+      title: "Generate controller",
+      task: () => createController(apiName),
+    },
+  ]);
+
+  await task.run();
 }
